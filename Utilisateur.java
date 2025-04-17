687e398473ce61ed46d6f0d100befc3444232b62
BazFouFou <105197334+BazFouFou@users.noreply.github.com>
2025-04-17 09:07:19 +0200

    Update Utilisateur.java

--- Utilisateur.java
+++ Utilisateur.java
@@ -1,18 +1,42 @@
-/**
- * debut du projet github et fin
- */
-abstract class Utilisateur {
-    protected String id;
-    protected String motDePasse;
+public abstract class Utilisateur {
+    protected int id; // idUtilisateur dans la BDD
     protected String nom;
+    protected String prenom;
+    protected String email;
+    protected String motDePasse;
+    protected String typeUtilisateur; // "client" ou "admin"
 
-    public Utilisateur(String id, String motDePasse, String nom) {
+    public Utilisateur(int id, String nom, String prenom, String email, String motDePasse, String typeUtilisateur) {
         this.id = id;
-        this.motDePasse = motDePasse;
         this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.typeUtilisateur = typeUtilisateur;
     }
 
-    public String getId() {
+    public int getIdUtilisateur() {
         return id;
     }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public String getTypeUtilisateur() {
+        return typeUtilisateur;
+    }
+
 }
