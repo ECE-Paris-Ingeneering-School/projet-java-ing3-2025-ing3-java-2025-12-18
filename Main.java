800ffb76521d712dfea2803e5c6aa0350f5cdd21
BazFouFou <105197334+BazFouFou@users.noreply.github.com>
2025-04-17 09:06:32 +0200

    Update Main.java
    Rediriger vers nouvelle page d'accueil

--- Main.java
+++ Main.java
@@ -4,24 +4,32 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // Création d'un client
-        Client client = new Client("client1", "password123", "Jean Dupont");
+        Client client = new Client(1, "Dupont", "Jean", "j@e", "1234");
 
         // Création d'articles
-        Article briquet = new Article("Briquet", "Bic", 0.50, 4.00, 10);
-        Article stylo = new Article("Stylo", "Parker", 1.20, 10.00, 10);
-
+        //Article briquet = new Article(1, "briquet", "Bic", 0.50, 4.00, 10, 30, 1);
+        //Article stylo = new Article(2, "stylo", "Parker", 1.20, 10.00, 10, 50, 2);
         // Création d'une commande
-        Commande commande = new Commande(client);
-        commande.ajouterArticle(briquet, 12);
-        commande.ajouterArticle(stylo, 5);
+        //Panier panier = new Panier(client);
+        //panier.ajouterArticle(briquet, 12);
+        //panier.ajouterArticle(stylo, 5);
 
-        // Ajout de la commande au client
-        client.ajouterCommande(commande);
 
-        // Affichage du total de la commande
-        System.out.println("Total de la commande: " + commande.calculerTotal() + " €");
+        // Insertion des articles dans la base de données
+        //ArticleDAO.addArticle(briquet);
+        //ArticleDAO.addArticle(stylo);
 
-                //Test connexion BDD
+        // Affichage de tous les articles en base
+        List<Article> liste = ArticleDAO.getAllArticles();
+        System.out.println("📦 Articles disponibles en base :");
+        for (Article a : liste) {
+            System.out.println(a);
+        }
+
+
+        new Accueil();
+
+        //Test connexion BDD
         try {
             var connexion = ConnexionBD.getConnexion();
             System.out.println("✅ Connexion réussie à la base de données !");
@@ -29,6 +37,7 @@ public class Main {
         } catch (Exception e) {
             System.err.println("❌ Erreur de connexion : " + e.getMessage());
         }
+
     }
 }
 
