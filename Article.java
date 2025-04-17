536e7ee4ba546f52ec206f1d33599cde2385aa79
BazFouFou <105197334+BazFouFou@users.noreply.github.com>
2025-04-17 09:04:01 +0200

    Update Article.java

--- Article.java
+++ Article.java
@@ -1,21 +1,69 @@
 public class Article {
-    private String nom;
-    private String marque;
+    private int idArticle;
+    private String nomArticle;
+    private String description;
     private double prixUnitaire;
     private double prixVrac;
     private int quantiteVrac;
+    private int stock;
+    private int idMarque;
 
-    public Article(String nom, String marque, double prixUnitaire, double prixVrac, int quantiteVrac) {
-        this.nom = nom;
-        this.marque = marque;
+    public Article(int idArticle, String nomArticle, String description, double prixUnitaire,
+                   double prixVrac, int quantiteVrac, int stock, int idMarque) {
+        this.idArticle = idArticle;
+        this.nomArticle = nomArticle;
+        this.description = description;
         this.prixUnitaire = prixUnitaire;
         this.prixVrac = prixVrac;
         this.quantiteVrac = quantiteVrac;
+        this.stock = stock;
+        this.idMarque = idMarque;
     }
 
-    public double calculerPrix(int quantite) {
+    public int getIdArticle() {
+        return idArticle;
+    }
+    public String getNomArticle() {
+        return nomArticle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrixUnitaire() {
+        return prixUnitaire;
+    }
+
+
+    public double getPrixVrac() {
+        return prixVrac;
+    }
+
+    public int getQuantiteVrac() {
+        return quantiteVrac;
+    }
+    public int getStock() {
+        return stock;
+    }
+    public int getIdMarque() {
+        return idMarque;
+    }
+
+public double calculerPrix(int quantite) {
         int lots = quantite / quantiteVrac;
         int reste = quantite % quantiteVrac;
         return (lots * prixVrac) + (reste * prixUnitaire);
     }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "id=" + idArticle +
+                ", nom='" + nomArticle + '\'' +
+                ", prixUnitaire=" + prixUnitaire +
+                ", stock=" + stock +
+                '}';
+    }
+
 }
